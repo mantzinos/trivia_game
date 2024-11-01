9cf5e2a81a34184d9f3336f726e0fca7efad58d1
makis <mantzinos@hotmail.com>
2024-11-01 13:31:46 +0200

    hover

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Answer = ({ answer }) => {
+  const [hover, setHover] = useState(false);
   return (
-    <div className="answer">
+    <div
+      onMouseOver={() => {
+        setHover(true);
+      }}
+      onMouseOut={() => {
+        setHover(false);
+      }}
+      style={{
+        backgroundColor: hover && "#112d4e",
+        border: hover && "2px solid #f9f7f7"
+      }}
+      className="answer"
+    >
       <h2>{answer}</h2>
     </div>
   );
